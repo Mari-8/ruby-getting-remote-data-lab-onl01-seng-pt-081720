93b50155d983209c364de1e0b1a07d384a861502
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-27 12:39:05 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,11 +5,11 @@ require 'open-uri'
 class GetRequester 
   
   def intitialize(url)
-    URI = url 
+    @URI = url 
   end 
   
   def get_response_body 
-    uri = URI.parse
+    uri = @URI.parse
     response = Net::HTTP.get_resonse(uri) 
     response.body 
   end 
