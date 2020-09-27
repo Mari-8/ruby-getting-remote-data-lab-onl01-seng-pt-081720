938dbb77edf28c65d188b5c01b358b3ba57d4467
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-27 13:05:35 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -13,7 +13,7 @@ class GetRequester
   
   def get_response_body 
     uri = URI.parse(@url)
-    response = Net::HTTP.get_resonse(uri)
+    response = Net::HTTP.get_response(uri)
     response.body 
   end 
   
